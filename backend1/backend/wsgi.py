77bb53d3b0cdee3db170c9b4f29abe2012addbb3
Mausam Shrestha <mausamshrestha1200@gmail.com>
2025-08-31 20:04:47 +0545

    updated wsgi

--- backend1/backend/wsgi.py
+++ backend1/backend/wsgi.py
@@ -1,16 +1,15 @@
 """
 WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend1.backend.settings')
+# Add the project root (backend1) to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Set the settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
