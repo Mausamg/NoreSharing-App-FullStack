41ca1c88eea1871a927a565eefd43b58bc9a0d6e
Mausam Shrestha <mausamshrestha1200@gmail.com>
2025-08-31 19:31:56 +0545

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -o errexit  # exit on error
 
-pip install -r requirements.txt
+pip install -r backend1/requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python backend1/manage.py collectstatic --no-input
+python backend1/manage.py migrate
