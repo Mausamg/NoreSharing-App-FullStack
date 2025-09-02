5c2b80703549937efb7542a680f853d9d3f17a5a
Mausam Shrestha <mausamshrestha1200@gmail.com>
2025-09-02 10:16:46 +0545

    update the urls

--- backend1/backend/urls.py
+++ backend1/backend/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     # JWT refresh endpoint for renewing access tokens
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
